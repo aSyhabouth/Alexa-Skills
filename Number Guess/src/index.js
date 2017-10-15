0e4ab80866d858d4c8ec95a2407fd821fafde755
asyhabouth <asyhabouth@my.uri.edu>
2017-10-15 17:41:27 -0400

    Added hypothetical code that would enable the skill to allow users to take another guess at a new number.

--- Number Guess/src/index.js
+++ Number Guess/src/index.js
@@ -8,7 +8,8 @@ var Alexa = require('alexa-sdk');
 const APP_ID = 'amzn1.ask.skill.bd00bf1a-4ee7-4515-aef7-f72d1688c61b';
 
 const STATES = {
-	GUESS: '_GUESSMODE' // Ask user to guess the number.
+	// START: '_STARTMODE',	// Restart the session if user wants
+	GUESS: '_GUESSMODE' 	// Ask user to guess the number.
 };
 
 exports.handler = function (event, context, callback) {
@@ -19,13 +20,32 @@ exports.handler = function (event, context, callback) {
 };
 
 //	Not connected to any states, this is the initial handler
-const newSessionHandlers = {
+//
+//	If restart session is going to happen, need to create a new boolean
+//		slot to determine if this is a new session or another session.
+//		For a more creative flair, instead of a boolean slot, a number
+//		slot can be made that way Alexa can track how many times the
+//		user has played during this one session.
+//	This restart function will assume that the same user that just played
+//		is playing again and there will be no switch of players
+const newSessionHandlers = Alexa.CreateStateHandler(STATES.START, {
 	'LaunchRequest': function() {
+		// if (!this.attributes['session'])		// For a number slot, if (['session'] > 0)
+			// this.emit('ReLaunchIntent');
 		this.emit('LaunchIntent');
 	},
 	'LaunchIntent': function() {
+		// this.attributes['session'] = false;	//	For a number slot, ['session'] += 1
 		this.emit(':ask', "Hello, what is your name?");
 	},
+	// 'ReLaunchIntent': function() {
+	//	var message = 'Welcome back, ' + this.attributes['userName'] + '.' +
+		//	' You have now tried guessing ' + this.attributes['session'] + ' times.' +	// ****
+	// 		' I have again thought of a number between one and one-hundred.' +
+	// 		' Please take a guess once again.';
+	// 	this.handler.states = STATES.GUESS;	// Shift state to guess
+	// 	this.emit(':ask', message);
+	// },
 	'nameIntent': function() {
 		this.attributes['userName'] = this.event.request.intent.slots.userName.value;
 		this.handler.state = STATES.GUESS;	//	Shift state to guess
@@ -39,6 +59,9 @@ const newSessionHandlers = {
 			'like to know who is playing.';
         this.emit(':ask', message);
     },
+	// 'AMAZON.YesIntent': function() {
+	// 	this.emit('LaunchRequest');
+	// },
 	'AMAZON.NoIntent': function() {
 		this.emit(':tell', 'Hopefully you try to guess my number next time.');
 	},
@@ -48,7 +71,7 @@ const newSessionHandlers = {
 	'AMAZON.StopIntent': function() {
 		this.emit(':tell', 'Hopefully you try to guess my number next time.');
 	}
-};
+});
 //	The only state within the skill, this is the state that handles the user's guess
 const guessStateHandlers = Alexa.CreateStateHandler(STATES.GUESS, {
 	'numberIntent': function() {
@@ -78,6 +101,9 @@ const guessStateHandlers = Alexa.CreateStateHandler(STATES.GUESS, {
 });
 
 //	This is a helper handler for the Guess State
+//	
+//	To implement the restart session function, the emit within correctGuess
+//		and wrongGuess would have to be tweaked abit
 const guessAttemptHandlers = {
 	'noGood': function() {
 		var message = 'What you have guessed is not valid. ' +
@@ -93,6 +119,11 @@ const guessAttemptHandlers = {
 			'playing. You had a great guess of ' + num1 + '. You have ' +
 			'guessed correctly! Congratulations!';
 		this.emit(':tell', message);
+		// var message = 'Thank you, ' + this.attributes['userName'] + ', for ' +
+		// 	'playing. You had a great guess of ' + num1 + '. You have ' +
+		// 	'guessed correctly! Congratulations! Would you like to play again?';
+		// this.handler.state = STATES.START;
+		// this.emit(':ask', message);
 	},
 	'wrongGuess': function(num1, num2) {
 		var large = (num1 > num2) ? num1 : num2;
@@ -102,5 +133,10 @@ const guessAttemptHandlers = {
 			'You had a great guess of ' + num1 + '. It was off by' + (large - small) +
 			'from what I was thinking of.';
 		this.emit(':tell', message);
+		// var message = 'Thank you, ' + this.attributes['userName'] + ', for playing. ' +
+		// 	'You had a great guess of ' + num1 + '. It was off by' + (large - small) +
+		// 	'from what I was thinking of. Would you like to play again?';
+		// this.handler.state = STATES.START;
+		// this.emit(':ask', message);
 	}
 };
